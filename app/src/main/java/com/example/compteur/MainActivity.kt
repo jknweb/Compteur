53078d910c4760cc9c35177901cf0cb318911384
Kalombo John <johnkalombofr@gmail.com>
2022-03-12 21:09:08 +0100

    Ajout interraction

--- app/src/main/java/com/example/compteur/MainActivity.kt
+++ app/src/main/java/com/example/compteur/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.compteur
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var buttonAction : Button
+    lateinit var counterTextView : TextView
+
+    var count : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonAction = findViewById(R.id.buttonAction)
+        counterTextView = findViewById(R.id.counter)
+
+        buttonAction.setOnClickListener {
+            count +=1
+            counterTextView.text = count.toString()
+        }
     }
 }
